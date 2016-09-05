82141f5edd419d3ee476d1f01f7bcba51386d692
Byron <byron@atebol.com>
2016-09-05 21:36:26 +0100

    scrollbar works

--- root/scripts/jquery.hades-scrollbar.js
+++ root/scripts/jquery.hades-scrollbar.js
@@ -12,6 +12,7 @@
     var $viewport;
     var $scrollable;
 
+
     /*---------------------- INITIALISATION ------------------------*/
 
     function App(el, opts){
@@ -49,9 +50,12 @@
         var content_height = get_content_height($scrollable);
         var view_height = $viewport.height();
 
-        console.log(current_position);
+        //console.log(view_height);
+        //console.log(current_position);
         console.log(content_height);
-        console.log($viewport.height());
+        console.log(view_height);
+
+        setup_handle_height(content_height, view_height);
 
         $viewport.scroll_event();
         $handle.add_drag_function_to_element();
@@ -65,11 +69,11 @@
             $(this).on('mousewheel', function(event) {
                 if(event.deltaY == -1){
                     console.log($scrollable.scrollTop());
-
+                    adjust_handle();
                 }
                 if(event.deltaY == 1){
                     console.log($scrollable.scrollTop());
-
+                    adjust_handle();
                 }
             });
         });
@@ -90,7 +94,7 @@
                 },
                 drag: function (event, ui) {
                     console.log("currently dragging");
-
+                    adjust_content();
                 },
                 stop: function (event, ui) {
                     console.log("stopped dragging");
@@ -110,6 +114,64 @@
 
 
     /*---------------------- PRIVATE FUNCTIONS ------------------------*/
+
+    function adjust_content(){
+
+        var current_position = parseInt($handle.css('top'));
+        var inner_track_height = $track.height() - $handle.height();
+        var position_percentage = calculate_percentage(current_position , inner_track_height);
+
+        var handle_position = parseInt(position_percentage/100 * get_inner_height());
+        console.log(handle_position);
+        $scrollable.scrollTop(handle_position);
+    }
+
+    function adjust_handle(){
+
+        var current_position = $scrollable.scrollTop();
+        var position_percentage = calculate_percentage(current_position , get_inner_height());
+        var inner_track_height = $track.height() - $handle.height();
+        var handle_position = position_percentage/100 * inner_track_height;
+        $handle.css('top', handle_position+"px");
+
+    }
+
+    function get_inner_height(){
+
+        var content_height = get_content_height($scrollable);
+        var view_height = $viewport.height();
+        var padding = get_content_margin_total($scrollable);
+        return content_height - view_height + padding;
+    }
+
+    function setup_handle_height(content_height, view_height){
+
+        var percentage = calculate_percentage(view_height, content_height);
+        var track_height = $track.height();
+        var handle_height = percentage / 100 * track_height;
+        $handle.css('height', handle_height);
+
+    }
+
+    function calculate_percentage(view_height, content_height) {
+        return (100 / content_height * view_height);
+    }
+
+    function get_content_margin_total($scroll_area){
+
+        var height = 0;
+
+        $scroll_area.find('*').each(function() {
+            height+= (get_margin(this, "top") + get_margin(this, "bottom"));
+        });
+
+        return height;
+
+    }
+
+    function get_margin(element, loc){
+        return  parseInt(window.getComputedStyle(element).getPropertyValue("margin-" + loc));
+    }
 
     function get_content_height($scroll_area){
 
